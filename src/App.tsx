cc6ef30327d2490aee51d5f909946ae0cdd84a59
PedroKcz <pedro.kcz@gmail.com>
2025-01-24 15:58:58 -0300

    Add scrollable goals

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,94 @@
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  KeyboardAvoidingView,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useState, useEffect, useRef } from 'react';
+import uuid from 'react-native-uuid';
+
+interface Goal {
+  text: string;
+  key: string;
+}
 
 const App = () => {
+  const flatListRef = useRef(null);
+
+  const [enteredGoalText, setEnteredGoal] = useState<string>('');
+  const [goals, setGoal] = useState<Goal[]>([]);
+
+  useEffect(() => {
+    flatListRef.current?.scrollToEnd({ animated: true });
+  }, [goals]);
+
+  const goalInputHandler = (enteredGoalText: string) => {
+    setEnteredGoal(enteredGoalText);
+  };
+
+  const addGoalHandler = () => {
+    setGoal((previousGoals) => [
+      ...previousGoals,
+      { text: enteredGoalText, key: uuid.v4() },
+    ]);
+    setEnteredGoal('');
+  };
+
   return (
     <SafeAreaView>
-      <View style={styles.appContainer}>
-        <View style={styles.inputContainer}>
-          <TextInput placeholder="Your goal" style={styles.textInput} />
-          <Button title="Add goal" />
+      <KeyboardAvoidingView>
+        <View>
+          <View style={styles.inputContainer}>
+            <TextInput
+              placeholder="Your goal"
+              style={styles.textInput}
+              value={enteredGoalText}
+              onChangeText={goalInputHandler}
+            />
+            <Button title="Add goal" onPress={addGoalHandler} />
+          </View>
+
+          <FlatList
+            data={goals}
+            ref={flatListRef}
+            renderItem={(data) => (
+              <View style={styles.goalItem}>
+                <Text>{data.item.text}</Text>
+              </View>
+            )}
+            contentContainerStyle={{ paddingVertical: 16 }}
+          />
         </View>
-        <View></View>
-      </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  appContainer: {
-    padding: 16,
-  },
   inputContainer: {
-    flexDirection: "row",
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: 'gray',
   },
   textInput: {
     borderWidth: 1,
-    borderColor: "gray",
+    borderColor: 'gray',
     flex: 1,
     marginEnd: 16,
+  },
+  goalItem: {
+    marginHorizontal: 16,
+    marginVertical: 8,
+    borderRadius: 6,
+    borderColor: 'gray',
+    padding: 16,
+    borderWidth: 1,
   },
 });
 
