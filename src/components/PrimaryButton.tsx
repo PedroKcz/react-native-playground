7ecf01fba4265fe04292e050e9caa974c31abe24
PedroKcz <pedro.kcz@gmail.com>
2025-02-03 19:39:41 -0300

    Add style to primary button

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -1,4 +1,4 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View, Platform } from 'react-native';
 
 const PrimaryButton = ({ children }) => {
   const pressHandler = () => {
@@ -6,25 +6,42 @@ const PrimaryButton = ({ children }) => {
   };
 
   return (
-    <Pressable onPress={pressHandler}>
-      <View style={styles.buttonContainer}>
-        <Text>{children}</Text>
-      </View>
-    </Pressable>
+    <View style={styles.buttonContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed && Platform.OS == 'ios'
+            ? [styles.pressableContainer, styles.pressedButton]
+            : styles.pressableContainer
+        }
+        android_ripple={{ color: 'gray' }}
+        onPress={pressHandler}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   buttonContainer: {
-    backgroundColor: 'mediumseagreen',
     borderRadius: 28,
-    paddingHorizontal: 16,
-    paddingVertical: 8,
+    backgroundColor: 'mediumseagreen',
+    overflow: 'hidden',
     elevation: 2,
     shadowColor: 'black',
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+  },
+  pressableContainer: {
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+  },
+  buttonText: {
+    textAlign: 'center',
+  },
+  pressedButton: {
+    opacity: 0.5,
   },
 });
 
